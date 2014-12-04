769353e6442258552ea1412ce532039d2208d736
Sam Corcos <samcorcos@gmail.com>
2014-12-04 17:23:05 -0500

    removed old code

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -7,36 +7,18 @@ Template.home.rendered = function() {
 		    .scale(1000)
 		    .translate([width / 2, height / 2]);
 
-
 		var path = d3.geo.path()
 		    .projection(projection);
-
 
 		var svg = d3.select("#kenyaMap").append("svg")
 		    .attr("width", width)
 		    .attr("height", height)
-		    // .append("rect")
-		    // .attr({x:10,y:10,height:100,width:100,fill:'orange'})
 
 		var centralPath =  "central.json";
 
-
-
-		// $.getJSON("./central.json", function(json) {
-
-  //  			console.log('JSON ',json); // this will show the info it in firebug console
-		// });
-			
-
-		// // var centralPath =  "../../../both/lib/central.json";
-		// console.log(centralPath)
-
-
 		d3.json(centralPath, function(error, kenya) {
 			central = kenya;
-			console.log('Kenya ',kenya)
 		  if (error) return console.error(error);
-
 
 		  svg.append("path")
 		      // .datum(topojson.feature(kenya, us.objects.subunits))
@@ -47,11 +29,15 @@ Template.home.rendered = function() {
 		  //     .data(topojson.feature(us, us.objects.subunits).features)
 		  //   .enter().append("path")
 
-		      
+
 		      .attr("d", path);
 
 		});
-}
+};
+
+
+
+
 Template.home.events({
 
 });
