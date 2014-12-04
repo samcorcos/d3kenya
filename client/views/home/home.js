b36f7c22fa0d516eaaec1b92ac5e9894bc60ca0b
Sam Corcos <samcorcos@gmail.com>
2014-12-03 19:06:42 -0500

    initial commit

---
+++ client/views/home/home.js
@@ -0,0 +1,16 @@
+Template.home.rendered = function() {
+
+};
+
+Template.home.events({
+	"click #page-close-button": function(e, t) {
+		e.preventDefault();
+		Router.go("", {});
+	},
+
+
+});
+
+Template.home.helpers({
+
+});
